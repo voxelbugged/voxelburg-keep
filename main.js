b3c095e6003c8b171eacd0c40a9e264ed40d77ca
voxelbugged <47631415+voxelbugged@users.noreply.github.com>
2023-09-11 15:51:32 +0200

    enemy logic
    real one

--- main.js
+++ main.js
@@ -12,13 +12,22 @@ var enemyAttack = 1;
 
 var enemyDefense = 0;
 
+var enemyNumber = 1;
+
+var gold = 0;
+
+var healPrice = 1;
+
 function fight() {
     health -= (enemyAttack - defense);
     enemyHealth -= (attack - enemyDefense);
-    updateStats()
     if (health <= 0){
         document.write("oh no your in die :((( so verry sad now refresh page to do the thing again");
     }
+    if (enemyHealth <= 0){
+        enemyDeath();
+    }
+    updateStats();
 }
 
 function updateStats() {
@@ -30,4 +39,23 @@ function updateStats() {
     document.getElementById("enemyMaxHealth").innerHTML = enemyMaxHealth;
     document.getElementById("enemyAttack").innerHTML = enemyAttack;
     document.getElementById("enemyDefense").innerHTML = enemyDefense;
+    document.getElementById("gold").innerHTML = gold;
+    document.getElementById("healPrice").innerHTML = healPrice;
+    document.getElementById("enemyNumber").innerHTML = enemyNumber;
+}
+
+function enemyDeath() {
+    gold += enemyNumber;
+    enemyNumber += 1;
+    enemyMaxHealth += 1;
+    enemyHealth = enemyMaxHealth;
+}
+
+function heal() {
+    if (gold >= healPrice){
+    health = maxHealth;
+    gold -= healPrice;
+    healPrice *= 2;
+    updateStats()
+    }
 }
